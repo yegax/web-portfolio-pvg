b223e9494315ca13219226b6845c6c6aa0bd754a
yegax <yegax.digital@gmail.com>
2021-07-17 19:20:56 -0400

    General ui changes (item size, order, text content, new list), added 2 new pages on dist directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
     colors: {
       'custom-orange': '#ffcc00'
@@ -10,7 +10,6 @@ module.exports = {
     }
   },
   variants: {
-    extend: {},
   },
   plugins: [],
 }
